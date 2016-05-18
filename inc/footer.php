59fdddc67abb8af0b55a308f84897be7e160973b
Ersin YIILDIZ <eersinyildizz@gmail.com>
2016-05-18 17:58:23 +0300

    footer updated

--- inc/footer.php
+++ inc/footer.php
@@ -1,13 +1,6 @@
 
 
-</div>
- 	</div>
- 	
- <footer class="footer">
- <div class="container">
- 	<p class="text-muted">Powered By <a href="https://github.com/eersinyildizz">Ersin Yıldız</a></p>
- </div>
- 	
- </footer>
+<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
 </body>
- </html
+</html>
