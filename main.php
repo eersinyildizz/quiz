1266a4c9e524cb656006827e57d6fc02bf7c459b
Ersin YIILDIZ <eersinyildizz@gmail.com>
2016-05-18 18:14:11 +0300

    main.php updated

--- main.php
+++ main.php
@@ -1,47 +1,13 @@
-<?php include "inc/header_main.php"; ?>
-<nav class="navbar navbar-inverse m_navbar" style=" border-color: #8CD1ED;">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#" style="color:black;">QUIZ</a>
-    </div>
-    <ul class="nav navbar-nav navbar-right">
-      <li class="active" ><a href="#" style="color:black; background-color:#9DE0EE;">Ersin Yıldız</a></li>
-      
-    </ul>
-  </div>
-</nav>
- 	<div class="container">
- 		<div class="jumbotron">
- 		<h3>Merhaba Ersin Yıldız,</h3>
- 		<div class="row">
- 			<div class="col-md-4" style="border:solid; border-color:rgba(85,191,208,0.3); border-radius:10px;">
- 				<h2 style="color:#18D8D2; "><i class="fa fa-bullhorn" style="color:#C5E8EF;"></i>  Tüm Quizler</h2>
-	 				<ul>
-	 					<li><a href="">zaaa</a></li>
-	 					<li><a href="">zaaa</a></li>
-	 					<li><a href="">zaaa</a></li>
-	 				</ul>
- 			</div>
- 			<div class="col-md-4" style="border:solid; border-color:rgba(85,191,208,0.3); border-radius:10px;">
- 				<h2 style="color:#18D8D2;"><i class="fa fa-users" style="color:#DFD7CC;"></i> Tüm Kullanıcılar</h2>
- 					<ul>
-	 					<li><a href="">zaaa</a></li>
-	 					<li><a href="">zaaa</a></li>
-	 					<li><a href="">zaaa</a></li>
-	 				</ul>
- 			</div>
- 			<div class="col-md-4" style="border:solid; border-color:rgba(85,191,208,0.3); border-radius:10px;">	
- 				<h2 style="color:#18D8D2;"><i class="fa fa-star" style="color:#DFD7CC;"></i> Quizlerim</h2>
- 					<ul>
-	 					<li><a href="">zaaa</a></li>
-	 					<li><a href="">zaaa</a></li>
-	 					<li><a href="">zaaa</a></li>
-	 				</ul>
- 			</div>
- 		</div>
+<?php 
+include "inc/header.php"; ?>
+	
+	<div class="container">
+		<div class="jumbotron">
+			<div class="alert alert-success">
+			  <strong><?=$_SESSION["name"] ?></strong> Welcome to My quiz
+			</div>
+			<a href="question.php"><button type="button" class="btn btn-primary btn-block btn-lg">Add Question</button></a><br>
+			<a href="ready.php"><button type="button" class="btn btn-success btn-block btn-lg">Start Quiz</button></a>
+		</div>
 	</div>
-	</div>
-	<center>
-	<a href="questions.php"><button type="button" class="btn btn-primary  btn-lg">Quiz Oluştur</button></a>
-	<a href="quiz.php"><button type="button" class="btn btn-success  btn-lg">Quiz Çöz</button></a>
-	</center>
+<?php include "inc/footer.php";?>
